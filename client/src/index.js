3f157a4e12eef78d1de5dc21a3d73311d446ea68
Mogkc <carson.mogk@cox.net>
2019-05-01 10:32:12 -0700

    added route for canvas game

--- client/src/index.js
+++ client/src/index.js
@@ -12,6 +12,7 @@ import Login from './pages/Login';
 import Profile from './pages/Profile';
 import Signup from './pages/Signup';
 import Navbar from './components/Navbar';
+import CanvasGame from './components/CanvasGame';
 
 // Here is if we have an id_token in localStorage
 if(localStorage.getItem("id_token")) {
@@ -27,6 +28,7 @@ ReactDOM.render(
             <Route exact path="/login" component={Login} />
             <Route exact path="/signup" component={Signup} />
             <Route exact path="/profile" component={Profile} />
+            <Route exact path="/game" component={CanvasGame} />
         </div>
     </Router>
     , document.getElementById('root')
