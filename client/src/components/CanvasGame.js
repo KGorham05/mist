5588d319f509f51a518c1a05cc6e5d247b97a727
William Diebolt <william@Williams-MacBook-Pro.local>
2019-05-02 11:55:54 -0700

    need moar code

--- client/src/components/CanvasGame.js
+++ client/src/components/CanvasGame.js
@@ -2,50 +2,14 @@ import React, { Component } from 'react';
 
 class CanvasGame extends Component {
     socket = this.props.socket;
-
+	
     state = {
         up: false,
         down: false,
         left: false,
-        right: false
+        right: false,
+		fire: false
     }
-
-    componentDidMount() {
-        this.socket.emit('new player');
-        setInterval(() => {
-            this.socket.emit('movement', this.state);
-        }, 1000 / 60);
-
-        const canvas = this.refs.canvasGame;
-        canvas.width = 800;
-        canvas.height = 600;
-
-        // Make it listen for key presses
-        canvas.tabIndex = 1000;
-
-        canvas.addEventListener(
-            "keydown", this.keyDown, false
-        );
-
-        canvas.addEventListener(
-            "keyup", this.keyUp, false
-        );
-
-        const context = canvas.getContext("2d");
-
-        this.socket.on('state', players => {
-            context.clearRect(0, 0, 800, 600);
-
-            for (var id in players) {
-                var player = players[id];
-                context.fillStyle = (player.predator) ? 'red' : 'green';
-                context.beginPath();
-                context.arc(player.x, player.y, 10, 0, 2 * Math.PI);
-                context.fill();
-            }
-        });
-    }
-
     keyDown = (event) => {
         console.log(event.keyCode);
         switch (event.keyCode) {
@@ -61,6 +25,9 @@ class CanvasGame extends Component {
             case 83: // S
                 this.setState({ down: true });
                 break;
+			case 32: // space
+				this.setState({fire: true});
+
             default: break;
         }
     }
@@ -79,10 +46,130 @@ class CanvasGame extends Component {
             case 83: // S
                 this.setState({ down: false });
                 break;
+			case 32: // space
+				this.setState({fire:false});
             default: break;
         }
     }
 
+	componentDidMount = () => {
+        this.socket.emit('new player');
+    	const canvas = this.refs.canvasGame;
+        canvas.width = 800;
+        canvas.height = 600;
+        const context = canvas.getContext("2d");
+        // Make it listen for key presses
+        canvas.tabIndex = 1000;
+        canvas.addEventListener("keydown", this.keyDown, false);
+        canvas.addEventListener("keyup", this.keyUp, false);
+
+//left pointing images are in row of index 1 in sheet
+var srcLeft = 1;
+//right facing images are in row of index 2 in sheet
+var srcRight = 2;
+var srcX=0;
+var srcY=0;
+//sprite sheet dimensions
+var sheetWidth = 256;
+var sheetHeight = 256;
+var cols = 4;
+var rows = 4
+//width and height of each frame:
+var width = sheetWidth / cols; //64
+var height = sheetHeight / rows //64
+
+var currentFrame = 0;
+
+var bulb = new Image();
+var pika = new Image();
+var char = new Image();
+bulb.src = "/static/bulb.png";
+pika.src = "/static/pika.png";
+char.src = "/static/char.png";
+socket.on('state', function (obj) {
+	context.clearRect(0, 0, 800, 600);
+	for (var id in obj.players) {
+		var player = obj.players[id];
+		if (player.lastPredator) {
+			if (player.predator) {
+				context.drawImage(pika, player.frameX * width, getSrcY(player.facing), width, height, player.x, player.y, width, height);
+			} else {
+				context.drawImage(char, player.frameX * width, getSrcY(player.facing), width, height, player.x, player.y, width, height);
+		
+			}
+		} else if (player.predator) {
+			context.drawImage(pika, player.frameX * width, getSrcY(player.facing), width, height, player.x, player.y, width, height);
+
+		} else {
+			console.log(player.facing);
+			
+			context.drawImage(bulb, player.frameX * width, getSrcY(player.facing), width, height, player.x, player.y, width, height);
+		}
+	}
+	for (var p in obj.projectiles) {
+		var prj = obj.projectiles[p];
+		context.fillStyle = 'red';
+		context.beginPath();
+		context.arc(prj.x, prj.y, 4, 0, 2 * Math.PI);
+		context.fill();
+	}
+});
+
+
+function getSrcY(facing){
+	if(facing.slice(0,4) == "left"){
+		return 1 * height;
+	}
+	if(facing.slice(0,5) == "right"){
+		return 2 * height;
+	}
+	if(facing == "up"){
+		return 3 * height;
+	}
+	if(facing == "down"){
+		return 0 * height;
+	}if(facing == "else"){
+		return 0 * height;
+	}
+	
+}
+
+
+
+	setInterval(function () {
+	//calc facing
+		if(this.state.right){
+			if(this.state.up){
+				this.setState({facing: "right-up"});
+			}
+			else if(this.state.down){
+				this.setState({facing: "right-down"});
+			}
+			else{
+				this.setState({facing: "right"});
+			}
+		}else if(this.state.left){
+			if(this.state.up){
+				this.setState({facing: "left-up"});
+			}
+			else if(this.state.down){
+				this.setState({facing: "left-down"});
+			}
+			else{
+				this.setState({facing: "left"});
+			}
+			}else if(this.state.up){
+				this.setState({facing: "up"});
+		}else if(this.state.down){
+			this.setState({facing: "down"});
+		}else{
+			this.setState({facing: "else"});
+		}
+	socket.emit('movement', this.state);
+	}, 1000 / 60);
+	    
+
+}
     render() {
         return <canvas id="canvas" ref="canvasGame"
             width={800} height={600}
@@ -95,5 +182,4 @@ class CanvasGame extends Component {
             } />
     }
 }
-
 export default CanvasGame;
