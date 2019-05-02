e0f0307a40e477986fbaeb4b4a5ed0ed964b9c1e
Natalie Brickley <natt_rose@yahoo.com>
2019-05-01 18:34:54 -0700

    added css to game page

--- client/src/components/CanvasGame.js
+++ client/src/components/CanvasGame.js
@@ -90,7 +90,9 @@ class CanvasGame extends Component {
                 {
                     width: "800px",
                     height: "600px",
-                    border: "5px solid black"
+                    border: "5px solid black",
+                    marginLeft: "120px",
+                    marginTop: "10px"
                 }
             } />
     }
